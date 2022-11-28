fd3e8df03b23ba0a2c54ef5adc214eb692dd7d56
lipa88-mk <lipa88@mail.ru>
2022-11-28 20:12:36 +0400

    add dev and prod scripts

---
+++ webpack.config.prod.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCss = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    // target: 'web',
+    // devtool: 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        // path: __dirname + '/public',
+        path: path.resolve(__dirname, 'public'),
+        clean: true, // очистка папки каждый раз при запуске
+        
+        filename: "main.js",
+        // filename: "main.[contenthash].js", - хеширование при сохранении
+        
+        assetModuleFilename: 'assets/[name][ext]',
+        // assetModuleFilename: 'assets/[name]-[hash][ext]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+              },
+              {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    MiniCss.loader, 
+                    "css-loader", 
+                    "sass-loader"
+                ],
+                // use: ["style-loader", "css-loader"],  prod
+              },
+            {
+                test: /\.(jpeg|jpg|png|svg|gif)$/i,
+                type: 'asset/resource'
+            }, 
+            {
+                test: /\.(ttf|woff2?)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[name].[ext]"
+                }
+            }
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        }),
+        new MiniCss({
+            filename: "style.css",
+          })
+    ],
+
+    devServer: {
+        compress: false,
+        open: true,
+        port: 5004,
+        // static: './public',
+        hot: true,
+    },
+};
