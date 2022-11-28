b7c96be8613ade1f5a64a9656fd597b3ae5beac2
lipa88-mk <lipa88@mail.ru>
2022-11-28 21:02:04 +0400

    optimization

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const MiniCss = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "production",
@@ -9,32 +8,70 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public'),
         clean: true,
-        filename: "main.[contenthash].js", 
-        assetModuleFilename: 'assets/[name]-[hash][ext]',
+        filename: "[contenthash].js",
+        assetModuleFilename: 'assets/[hash][ext]',
     },
     module: {
         rules: [
             {
                 test: /\.html$/i,
                 loader: "html-loader",
-              },
-              {
+            },
+            {
                 test: /\.(c|sa|sc)ss$/i,
                 use: [
-                    "style-loader", 
-                    "css-loader", 
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    "postcss-preset-env",
+                                    // "postcss-deadcss",
+                                    // "at-rule-packer",
+                                    // "postcss-import"
+                                ],
+                            },
+                        },
+                    },
                     "sass-loader"
                 ],
-              },
+            },
             {
                 test: /\.(jpeg|jpg|png|svg|gif)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          mozjpeg: {
+                            progressive: true,
+                          },
+                          // optipng.enabled: false will disable optipng
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                          },
+                          gifsicle: {
+                            interlaced: false,
+                          },
+                          // the webp option will enable WEBP
+                          webp: {
+                            quality: 75
+                          }
+                        }
+                      },
+                ],
                 type: 'asset/resource'
-            }, 
+            },
             {
                 test: /\.(ttf|woff2?)$/i,
                 type: "asset/resource",
                 generator: {
-                    filename: "fonts/[name]-[hash].[ext]"
+                    filename: "fonts/[hash].[ext]"
                 }
             }
         ],
@@ -43,17 +80,13 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
-        }),
-        // new MiniCss({
-        //     filename: "[name].[contenthash].css",
-        //   })
+        })
     ],
 
     devServer: {
         compress: false,
         open: true,
-        port: 5004,
-        // static: './public',
         hot: true,
+        port: 8081,
     },
 };
